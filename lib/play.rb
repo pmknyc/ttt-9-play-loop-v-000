be959bced3ef5c09efb84e53036b1fcf50c43b58
Paula McKinley <pmknyc@optimum.net>
2018-08-20 22:15:46 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,11 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  play_count = 0
+    while play_count.between?(0,8)
+      turn(board)
+      play_count += 1
+      # See counter: puts "play_count = #{play_count}"
+    end
+end
